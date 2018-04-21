61cb73428353e6a2060268e27bd004c2c7adc759
Aren Villanueva <arenvillanueva@yomogi-soft.com>
2018-04-21 10:49:16 +1000

    Skeleton files for Windows Media Foundation streaming

---
+++ modules/wmf/register_types.cpp
@@ -0,0 +1,22 @@
+#include "register_types.h"
+#include "windows_media_foundation_video.h"
+#include "resource_importer_wmf_video.h"
+
+#include <mfapi.h>
+#include <stdio.h>
+
+void register_wmf_types() {
+    MFStartup(MF_VERSION, MFSTARTUP_FULL);
+
+#ifdef TOOLS_ENABLED
+	Ref<ResourceImporterWMFVideo> wmfv_import;
+	wmfv_import.instance();
+	ResourceFormatImporter::get_singleton()->add_importer(wmfv_import);
+#endif
+    ClassDB::register_class<VideoStreamWMF>();
+//    ClassDB::register_class<VideoStreamPlaybackWMF>();
+}
+
+void unregister_wmf_types() {
+    MFShutdown();
+}
